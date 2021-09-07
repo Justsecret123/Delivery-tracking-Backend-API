1faa1aaed4f109575585b8f3950e60451e794279
JustSecret123 <ibrahimserouis99@hotmail.com>
2021-09-07 15:06:40 +0100

    Restrcuturation

--- routes/deliverymen.js
+++ routes/deliverymen.js
@@ -13,6 +13,15 @@ router.get("/", (req,res)=> {
     res.status(200).json("Welcome to the main route :D");
 });
 
+//Route: get all deliverymen 
+router.get("/list", (req,res)=> {
+
+    DeliveryMan.find((err,result)=>{
+        res.status(200).json({message: "Success!", deliveryMen: result});
+    }).catch(err => res.status(400).json({message: "An error occured: " + err}));
+    
+});
+
 //Route: id 
 router.get("/:id", (req,res)=> {
 
@@ -33,15 +42,6 @@ router.get("/:id", (req,res)=> {
         res.status(400).json({message: "Bad request id formatting!"});
     }
 
-});
-
-//Route: get all deliverymen 
-router.get("/list", (req,res)=> {
-
-    DeliveryMan.find((err,result)=>{
-        res.status(200).json({message: "Success!", deliveryMen: result});
-    }).catch(err => res.status(400).json({message: "An error occured: " + err}));
-    
 });
 
 //Route : search by 'nom' or 'prenom'
